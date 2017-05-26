d9993834037ad819818c4840b5b9c101c0e19498
neozl24 <neozl24@hotmail.com>
2017-05-26 15:42:45 +0800

    complete shopcart

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <div class="shopcart">
-    <div class="content">
+    <div class="content" @click="toggleList">
       <div class="content-left">
         <div class="logo-wrapper">
           <div class="logo" :class="{'highlight': totalCount > 0}">
@@ -11,7 +11,7 @@
         <div class="price" :class="{'highlight': totalCount > 0}">¥{{totalPrice}}元</div>
         <div class="desc">另需配送费¥{{deliveryPrice}}</div>
       </div>
-      <div class="content-right">
+      <div class="content-right" @click.stop.prevent="pay">
         <div class="pay" :class="payClass">{{payDesc}}</div>
       </div>
     </div>
@@ -20,10 +20,33 @@
         <div class="inner inner-hook"></div>
       </div>
     </div>
+    <div class="shopcart-list" v-show="listShow" transition="fold">
+      <div class="list-header">
+        <span class="title">购物车</span>
+        <span class="empty" @click="empty">清空</span>
+      </div>
+      <div class="list-content" v-el:list-content>
+        <ul>
+          <li class="food" v-for="food in selectFoods">
+            <span class="name">{{food.name}}</span>
+            <div class="price">
+              <span>¥{{food.price*food.count}}</span>
+            </div>
+            <div class="cartcontrol-wrapper">
+              <cartcontrol :food="food"></cartcontrol>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
+  <div class="list-mask" @click="hideList" v-show="listShow" transition="fade"></div>
 </template>
 
 <script>
+import cartcontrol from 'components/cartcontrol/cartcontrol';
+import BScroll from 'better-scroll';
+
 export default {
   props: {
     selectFoods: {
@@ -63,7 +86,8 @@ export default {
           show: false
         }
       ],
-      dropBalls: []
+      dropBalls: [],
+      fold: true
     };
   },
   computed: {
@@ -94,6 +118,25 @@ export default {
     },
     payClass() {
       return this.totalPrice < this.minPrice ? 'not-enough' : 'enough';
+    },
+    listShow() {
+      if (this.totalCount === 0) {
+        this.fold = true;
+        return false;
+      } else {
+        if (!this.fold) {
+          this.$nextTick(() => {
+            if (!this.scroll) {
+              this.scroll = new BScroll(this.$els.listContent, {
+                click: true
+              });
+            } else {
+              this.scroll.refresh();
+            }
+          });
+        }
+        return !this.fold;
+      }
     }
   },
   methods: {
@@ -150,111 +193,203 @@ export default {
           }
         }
       }
+    },
+    toggleList() {
+      if (this.totalCount === 0) {
+        return;
+      } else {
+        this.fold = !this.fold;
+      }
+    },
+    hideList() {
+      this.fold = true;
+    },
+    empty() {
+      this.selectFoods.forEach((food) => {
+        food.count = 0;
+      });
+    },
+    pay() {
+      if (this.totalPrice < this.minPrice) {
+        return;
+      }
+      window.alert(`支付${this.totalPrice}元`);
     }
+  },
+  components: {
+    cartcontrol
   }
 };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-    .shopcart
-      position: fixed
-      left: 0
-      bottom: 0
-      z-index: 50
-      width: 100%
-      height: 48px
-      background: #999
-      .content
-        display: flex
-        background: #141d27
-        font-size: 0
-        color: rgba(255, 255, 255, 0.4)
-        .content-left
-          flex: 1
-          .logo-wrapper
-            display: inline-block
-            vertical-align: top
-            position: relative
-            top: -10px
-            margin: 0 12px
-            padding: 6px
-            width: 56px
-            height: 56px
-            box-sizing: border-box
+  @import "../../common/stylus/mixin.styl"
+
+  .shopcart
+    position: fixed
+    left: 0
+    bottom: 0
+    z-index: 50
+    width: 100%
+    height: 48px
+    background: #999
+    .content
+      display: flex
+      background: #141d27
+      font-size: 0
+      color: rgba(255, 255, 255, 0.4)
+      .content-left
+        flex: 1
+        .logo-wrapper
+          display: inline-block
+          vertical-align: top
+          position: relative
+          top: -10px
+          margin: 0 12px
+          padding: 6px
+          width: 56px
+          height: 56px
+          box-sizing: border-box
+          border-radius: 50%
+          background-color: #141d27
+          .logo
+            width: 100%
+            height: 100%
             border-radius: 50%
-            background-color: #141d27
-            .logo
-              width: 100%
-              height: 100%
-              border-radius: 50%
-              background-color: #2b343c
-              text-align: center
-              &.highlight
-                background-color: rgb(0, 160, 220)
-              .icon-shopping_cart
-                line-height: 44px
-                font-size: 24px
-                color: #80858a
-                &.highlight
-                  color: #fff
-            .num
-              position: absolute
-              top: 0
-              right: 0
-              width: 24px
-              height: 16px
-              line-height: 16px
-              text-align: center
-              border-radius: 16px
-              font-size: 9px
-              font-weight: 700
-              color: #fff
-              background-color: rgb(240, 20, 20)
-              box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4)
-          .price
-            display: inline-block
-            vertical-align: top
-            margin-top: 12px
-            line-height: 24px
-            padding-right: 12px
-            box-sizing: border-box
-            border-right: 1px solid rgba(255, 255, 255, 0.1)
-            font-size: 16px
-            font-weight: 700
-            &.highlight
-              color: #fff
-          .desc
-            display: inline-block
-            vertical-align: top
-            margin: 12px 0 0 12px
-            line-height: 24px
-            font-size: 10px
-        .content-right
-          flex: 0 0 105px
-          width: 105px
-          .pay
-            height: 48px
-            line-height: 48px
+            background-color: #2b343c
             text-align: center
+            &.highlight
+              background-color: rgb(0, 160, 220)
+            .icon-shopping_cart
+              line-height: 44px
+              font-size: 24px
+              color: #80858a
+              &.highlight
+                color: #fff
+          .num
+            position: absolute
+            top: 0
+            right: 0
+            width: 24px
+            height: 16px
+            line-height: 16px
+            text-align: center
+            border-radius: 16px
+            font-size: 9px
+            font-weight: 700
+            color: #fff
+            background-color: rgb(240, 20, 20)
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4)
+        .price
+          display: inline-block
+          vertical-align: top
+          margin-top: 12px
+          line-height: 24px
+          padding-right: 12px
+          box-sizing: border-box
+          border-right: 1px solid rgba(255, 255, 255, 0.1)
+          font-size: 16px
+          font-weight: 700
+          &.highlight
+            color: #fff
+        .desc
+          display: inline-block
+          vertical-align: top
+          margin: 12px 0 0 12px
+          line-height: 24px
+          font-size: 10px
+      .content-right
+        flex: 0 0 105px
+        width: 105px
+        .pay
+          height: 48px
+          line-height: 48px
+          text-align: center
+          font-size: 14px
+          font-weight: 700
+          &.not-enough
+            background-color: #2b333b
+          &.enough
+            background-color: #00b43c
+            color: #fff
+    .ball-container
+      .ball
+        position: fixed
+        left: 32px
+        bottom: 22px
+        z-index: 200
+        &.drop-transition
+          transition: all 0.4s cubic-bezier(0.49,-0.29,0.75,0.41)
+          .inner
+            width: 16px
+            height: 16px
+            border-radius: 50%
+            background-color: rgb(0, 160, 220)
+            transition: all 0.4s linear
+    .shopcart-list
+      position: absolute
+      left: 0
+      top: 0
+      z-index: -1
+      width: 100%
+      &.fold-transition
+        transition: all 0.5s
+        transform: translate3d(0, -100%, 0)
+      &.fold-enter, &.fold-leave
+        transform: translate3d(0, 0, 0)
+      .list-header
+        height: 40px
+        line-height: 40px
+        padding: 0 18px
+        background: #f3f5f7
+        border-bottom: 1px solid rgba(7,17,27,0.1)
+        .title
+          float: left
+          font-size: 14px
+          color: rgb(7, 17, 27)
+        .empty
+          float: right
+          font-size: 12px
+          color: rgb(0, 160, 220)
+      .list-content
+        padding: 0 18px
+        max-height: 217px
+        overflow: hidden
+        background: #fff
+        .food
+          position: relative
+          padding: 12px 0
+          box-sizing: border-box
+          border-1px(rgba(7,17,27,0.1))
+          .name
+            line-height: 24px
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .price
+            position: absolute
+            right: 90px
+            bottom: 12px
+            line-height: 24px
             font-size: 14px
             font-weight: 700
-            &.not-enough
-              background-color: #2b333b
-            &.enough
-              background-color: #00b43c
-              color: #fff
-      .ball-container
-        .ball
-          position: fixed
-          left: 32px
-          bottom: 22px
-          z-index: 200
-          &.drop-transition
-            transition: all 0.4s cubic-bezier(0.49,-0.29,0.75,0.41)
-            .inner
-              width: 16px
-              height: 16px
-              border-radius: 50%
-              background-color: rgb(0, 160, 220)
-              transition: all 0.4s linear
+            color: rgb(240, 20, 20)
+          .cartcontrol-wrapper
+            position: absolute
+            right: 0
+            bottom: 6px
+  .list-mask
+    position: fixed
+    top: 0
+    left: 0
+    width: 100%
+    height: 100%
+    z-index: 40
+    backdrop-filter: blur(10px)
+    &.fade-transition
+      transition: all 0.5s
+      opacity: 1
+      background-color: rgba(7, 17, 27, 0.6)
+    &.fade-enter, &.fade-leave
+      opacity: 0
+      background-color: rgba(7, 17, 27, 0)
 </style>
