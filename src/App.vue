c70f9c5fe578328619a2be1795ffed9c5fe5f15c
zhongli <neozl24@hotmail.com>
2017-06-26 17:49:28 +0800

    add seller component

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,53 @@
 <template>
-<div>
-  <v-header v-bind:seller="seller"></v-header>
-  <div class="tab">
-    <div class="tab-item">
-      <a v-link="{path: '/goods'}">商品</a>
+  <div>
+    <v-header v-bind:seller="seller"></v-header>
+    <div class="tab">
+      <div class="tab-item">
+        <a v-link="{path: '/goods'}">商品</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path: '/ratings'}">评论</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path: '/seller'}">商家</a>
+      </div>
     </div>
-    <div class="tab-item">
-      <a v-link="{path: '/ratings'}">评论</a>
-    </div>
-    <div class="tab-item">
-      <a v-link="{path: '/seller'}">商家</a>
-    </div>
+    <router-view :seller="seller"></router-view>
   </div>
-  <router-view :seller="seller"></router-view>
-</div>
 </template>
 
 <script>
-import header from 'components/header/header.vue';
+  import {urlParse} from 'common/js/util';
+  import header from 'components/header/header.vue';
 
-const ERR_OK = 0;
+  const ERR_OK = 0;
 
-export default {
-  data() {
-    return {
-      seller: {}
-    };
-  },
-  created() {
-    this.$http.get('/api/seller').then((response) => {
-      response = response.body;
-      if (response.errno === ERR_OK) {
-        this.seller = response.data;
-        // console.log(this.seller);
-      }
-    });
-  },
-  components: {
-    'v-header': header
-  }
-};
+  export default {
+    data() {
+      return {
+        seller: {
+          id: (() => {
+            let queryParam = urlParse();
+            // console.log(queryParam);
+            return queryParam.id;
+          })()
+        }
+      };
+    },
+    created() {
+      this.$http.get('/api/seller?id=' + this.seller.id).then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          // this.seller = response.data;   // 用这种方式会覆盖自定义的id属性
+          this.seller = Object.assign({}, this.seller, response.data);
+          console.log(this.seller.id);
+        }
+      });
+    },
+    components: {
+      'v-header': header
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
